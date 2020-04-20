c256d0d3ed8fa15cb4bc2ae526b05ce1ad5d19d6
harringtonsp1 <harringtonsp1@gmail.com>
2020-04-19 22:20:29 -0400

    add FSM

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms.BaseAtoms;
+using UnityAtoms.FSM;
+using UnityAtoms.Tags;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    CharacterController controller;
+
+    [SerializeField] private FiniteStateMachineReference StateMachineRef;
+
+    [SerializeField] private StringConstant playerTag;
+
+    [SerializeField]
+    private FloatReference _shootingRange = new FloatReference (1f);
+
+    GameObject player;
+
+    [SerializeField] private float speed = 10f;
+    void Awake ()
+    {
+        gameObject.TryGetComponent<CharacterController> (out controller);
+
+        AtomTags.OnInitialization (() => player = AtomTags.FindByTag (playerTag.Value));
+
+        StateMachineRef.Machine.OnUpdate ((deltaTime, value) =>
+        {
+            if (player != null)
+            {
+                var distance = Vector3.Distance (player.transform.position, transform.position);
+
+                MoveTowardsPlayer ();
+            }
+            else
+            {
+
+            }
+        }, gameObject);
+
+        StateMachineRef.Machine.DispatchWhen (
+            command: "ATTACK",
+            (value) => player != null && value == "CHASING" && (_shootingRange.Value >= Vector3.Distance (player.transform.position, transform.position)),
+            gameObject
+        );
+
+        StateMachineRef.Machine.DispatchWhen (
+            command: "CHASE", (value) => player.transform != null && value == "ATTACKING" && (_shootingRange.Value < Vector3.Distance (player.transform.position, transform.position)), gameObject);
+    }
+
+    void MoveTowardsPlayer ()
+    {
+        controller.SimpleMove ((player.transform.position - transform.position).normalized * speed);
+    }
+}
